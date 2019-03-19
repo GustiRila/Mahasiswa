ee49ea0ac7ef46d62a1110d8c5042160cda10247
GustiRila <praja.rila31@gmail.com>
2019-03-19 14:58:24 +0700

    oke

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek dari class Mahasiswa 
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //mengeset nilai properties objek saya dan kamu
+            saya.Nim = "1871265";
+            saya.Nama = "Gusti";
+            saya.Ipk = 3.92f;
+
+            kamu.Nim = "0812235";
+            kamu.Nama = "Oke";
+            kamu.Ipk = 3.65f;
+
+            //memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
